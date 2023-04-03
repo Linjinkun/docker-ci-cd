0ede643af3d2984f9b95fd4ed16b611a47fe3847
Linjinkun <380993970@qq.com>
2023-04-04 01:16:19 +0800

    Update index.php

--- demo/index.php
+++ demo/index.php
@@ -1,3 +1,14 @@
 <?php
-echo 144423;
+$servername = "mysql5"; // 服务器名称   
+$username = "root"; // 用户名    
+$password = "123456"; // 密码  
+
+// 创建连接
+$conn = new mysqli($servername, $username, $password);
+
+// 检测连接
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);
+}
+echo "连接成功";
 ?>
